c0aa757f10dd367a7a23c14240c23f4339431c1a
loulou <loulou@sun.net.tw>
2020-08-29 16:58:46 +0800

    set server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const app = express();
+
 app.use(express.static(_dirname + '/dist/first-angular') );
 app.get('/*',function(req,res){
 	res.sendFile(
