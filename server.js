6baf68dda5ab68a114e8a049ebdd918284849d0c
loulou <loulou@sun.net.tw>
2020-08-29 17:59:07 +0800

    change __dirname

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+console.log('__dirname：', __dirname);
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/first-angular'));
+app.use(express.static(__dirname + '/'));
 app.get('/*', function(req,res) {
-	res.sendFile(path.join(__dirname+'/dist/first-angular/src/index.html'));
+	res.sendFile(path.join(__dirname+'/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
